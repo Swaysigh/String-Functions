c13ffcda4d304fb5f085ff6a8918b36b04b7a812
swaysigh <assentioncreed3@gmail.com>
2024-04-07 19:44:05 +0530

    added code

---
+++ strsub.c
@@ -0,0 +1,9 @@
+void substr(char str1[]; char result[]; int start; int end)
+{
+    int i, j;
+    while (i = start; j = 0; j < end; i++; j++)
+    {
+        result[j] = str1[i];
+    }
+    result[j] = '\0';
+}
